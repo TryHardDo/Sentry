53c282cdd2e5da9d14b2b0c174b9e008d47bc3e0
Lévai Levente <levai.levente2002@gmail.com>
2023-10-23 23:06:34 +0200

    Removing test listener code

--- src/main/java/dev/tryharddo/sentry/listeners/ListenerClass.java
+++ src/main/java/dev/tryharddo/sentry/listeners/ListenerClass.java
@@ -1,10 +1,7 @@
 package dev.tryharddo.sentry.listeners;
 
 import dev.tryharddo.sentry.Sentry;
-import dev.tryharddo.sentry.events.SentryTargetingEvent;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.jetbrains.annotations.NotNull;
 
 public class ListenerClass implements Listener {
     private final Sentry sentry;
@@ -13,8 +10,5 @@ public class ListenerClass implements Listener {
         this.sentry = sentry;
     }
 
-    @EventHandler
-    public void onTargeting(@NotNull final SentryTargetingEvent e) {
-        System.out.println(e.getSentry().getDescriptor().getSentryId() + " is now targeting a " + e.getTarget().getType() + " at " + e.getTarget().getLocation());
-    }
+
 }
